db80628c2b6c70989997a3247ff8e3080f5528da
Blake Martinson <blake.martinson@gmail.com>
2017-01-15 17:19:06 -0500

    06; Add App component and Header, Order, and Inventory

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,11 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 import './css/style.css';
 
 render(
-  <StorePicker />,
+  <App />,
   document.querySelector('#main')
 );
