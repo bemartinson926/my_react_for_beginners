e0b55df12e74db762fab766b6755e9af5f5d3ef4
Blake Martinson <blake.martinson@gmail.com>
2017-01-14 19:10:13 -0500

    03; Extract StorePicker to own file

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,12 +1,7 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-
-class StorePicker extends React.Component {
-  render() {
-    return <p>Hello</p>
-  }
-}
+import StorePicker from './components/StorePicker';
 
 render(
   <StorePicker />,
