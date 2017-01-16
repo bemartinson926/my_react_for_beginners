8aa64ca4bd141e39e6f3cd8dbec4fac75ab12193
Blake Martinson <blake.martinson@gmail.com>
2017-01-15 19:42:31 -0500

    10; Add defaultValue for storepicker with random name

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
@@ -8,7 +9,8 @@ class StorePicker extends React.Component {
         <input
           type="text"
           required
-          placeholder="Store Name" />
+          placeholder="Store Name"
+          defaultValue={getFunName()} />
         <button type="submit">Visit Store -></button>
       </form>
     )
