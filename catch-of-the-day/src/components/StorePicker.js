2f9995dfe954b308b9d633f8e89c2cb90cdd2ad7
Blake Martinson <blake.martinson@gmail.com>
2017-01-16 12:08:45 -0500

    11; handle submit on store picker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    // first grab text from the input
+    console.log(this.storeInput);
+    console.log('you changed the url')
+    // second transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => {this.goToStore(e)}}>
+      {/*
+        Requires constructor function to bind this
+        <form className="store-selector" onSubmit={this.goToStore}>
+
+        binds this
+        <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      */}
         <h2>Please Enter A Store</h2>
         <input
           type="text"
           required
           placeholder="Store Name"
-          defaultValue={getFunName()} />
+          defaultValue={getFunName()}
+          ref={ (input) => {this.storeInput = input} } />
         <button type="submit">Visit Store -></button>
       </form>
     )
