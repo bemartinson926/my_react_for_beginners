dbed5baa0dd36701444c8f53ef9375ac26eb2aa2
Blake Martinson <blake.martinson@gmail.com>
2017-01-17 12:31:00 -0500

    15; Add Fish component; add button to load sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes.js';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
 
     this.state = {
       fishes: {},
@@ -23,14 +26,32 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
+    const fishKeys = Object.keys(this.state.fishes);
+
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              fishKeys.length ?
+                fishKeys.map((fish) => {
+                  return <Fish key={fish} details={this.state.fishes[fish]} />;
+                })
+              :
+                <p>No fIsh</p>
+            }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory loadSamples={this.loadSamples} addFish={this.addFish} />
       </div>
     )
   }
